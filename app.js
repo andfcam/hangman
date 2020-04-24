6be27713d7ffa66c17f7e4aaa4400f81629cf669
elkdev <afcameron@gmail.com>
2020-04-24 19:26:58 +0100

    Added HTML elements for word and misses. Created a variable for the word to guess.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 window.onload = () => {
-    console.log("JS working correctly");
+    const wordToGuess = "hangman".toUpperCase();
+    console.log(wordToGuess);
 };
+
+// Start with 'HANGMAN' as fixed word
+// User can input a key as a guess
+// Check if the input letter is in the word
+// If it is - display it
+// If it isn't - increase no. of misses
+// If you miss 6 times, it's game over
