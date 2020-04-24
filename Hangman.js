36f5737d3aa8613e655d72451d917a9cf5c72d2c
elkdev <afcameron@gmail.com>
2020-04-24 22:23:42 +0100

    Added a setup function that displays underscores on the page to represent the letters to guess.

--- Hangman.js
+++ Hangman.js
@@ -2,11 +2,22 @@ class Hangman {
     constructor(word) {
         this.word = word;
         this.lettersInWord = word.toUpperCase().split("");
+
+        this.domWord = document.getElementById("word");
+        this.domMisses = document.getElementById("misses");
     }
 
     start = () => {
-        document.onkeypress = (event) =>
+        this.setup();
+        document.onkeypress = (event) => {
             this.guess(String.fromCharCode(event.keyCode));
+        };
+    };
+
+    setup = () => {
+        this.lettersInWord.forEach((letter) => {
+            this.domWord.innerHTML += `<span>_</span>`;
+        });
     };
 
     guess = (letter) => {
@@ -17,3 +28,7 @@ class Hangman {
         }
     };
 }
+
+// If it is - display it
+// If it isn't - increase no. of misses
+// If you miss 6 times, it's game over
--- app.js
+++ app.js
@@ -2,7 +2,3 @@ window.onload = () => {
     const game = new Hangman("hangman");
     game.start();
 };
-
-// If it is - display it
-// If it isn't - increase no. of misses
-// If you miss 6 times, it's game over
